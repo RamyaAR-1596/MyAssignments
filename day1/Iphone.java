d45be6ae47a885d06fff21de90740737781495ae
RamyaAR-1596 <134146756+RamyaAR-1596@users.noreply.github.com>
2023-05-20 18:59:11 +0530

    Add files via upload
    Day1

---
+++ day1/Iphone.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Iphone {
+
+	public void makecall() {
+		System.out.println("call");
+	}
+	public void makemodel() {
+		System.out.println("iphone");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Iphone I = new Iphone();
+		I.makecall();
+		I.makemodel ();
+	}
+
+}

40b53279e8c5931507f33b77e8360a048d190be7
RamyaAR-1596 <134146756+RamyaAR-1596@users.noreply.github.com>
2023-06-01 21:38:08 +0530

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Car {
+
+		public static void main(String[] args) {
+			int wheels =4;
+			boolean petrol=true;
+			String Brand = "KIA" ;
+			System.out.println("No of wheels" + wheels);
+			System.out.println("Car fuel" + petrol);
+			System.out.println("Car brand name" + Brand );
+
+		}
+	}
+
